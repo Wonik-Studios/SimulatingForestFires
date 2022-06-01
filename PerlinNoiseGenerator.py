f4300639c25f0630d41d32b87e60c92aea7e10b4
MobBarley2021 <nathank0707@gmail.com>
2022-06-01 12:37:22 -0400

    added functionality for saving the output of the PerlinNoisefunction

--- PerlinNoiseGenerator.py
+++ PerlinNoiseGenerator.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import matplotlib.pyplot as plt
 from perlin_noise import PerlinNoise
 
@@ -5,8 +6,10 @@ from perlin_noise import PerlinNoise
 def generate_noise(xpix, ypix):
     noise = PerlinNoise(octaves=10, seed=1)
     pic = [[noise([i/xpix, j/ypix]) for j in range(xpix)] for i in range(ypix)]
+    now = datetime.now()
+
     plt.imshow(pic, cmap='gray')
-    plt.show()
+    plt.savefig(f"LandscapeGraphs/{now.strftime('%m-%d-%Y, %H:%M:%S')}")
     return pic
 
 if __name__ == "__main__":
