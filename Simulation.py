8be2ba919a693250dab0a05bc350af317f3c3bc4
MobBarley2021 <79928013+MobBarley2021@users.noreply.github.com>
2022-06-01 11:14:31 -0400

    Create Simulation.py

---
+++ Simulation.py
@@ -0,0 +1,110 @@
+import pygame
+import numpy as np
+import math
+import random
+from PerlinNoiseGenerator import generate_noise
+
+
+
+class Simulation:
+    # green(0): represents the cells with vegetation that can be burned
+    # red(1): represents the cells with vegetation that is currently burning
+    # black(2): represents the cells that doesn't contain any vegetation
+    # gray(3): represents the cells that have already burnt
+    colors = {"black": (0, 0, 0), "green": (11, 193, 17), "red": (194, 13, 13),
+            "grey": (128, 128, 128)
+                }
+
+
+    def __init__(self, width: int, height: int, block_size: int, settings):
+        # instantiating variables width and height of the screen
+        self.height = height
+        self.width = width
+
+        # block_size is the length and width that each cell takes up
+        self.block_size = block_size
+        self.settings = settings
+
+        # with the height, width and block_size,
+        # we can calculate the number of blocks that go in every column and row
+        self.num_cols = math.ceil(self.width/self.block_size)
+        self.num_rows = math.ceil(self.height/self.block_size)
+
+        # We can calculate the horizontal and vertical wind by 
+        # considering the wind-power in conjunction with the wind-angle to be
+        # vectors in 2d space. We can use a bit of trig to find it.
+        # the horizontal_wind is positive when there is wind to the right
+        self.horizontal_wind = 0 if settings["wind-power"] == 0 else math.sin(settings["wind-angle"]) / settings["wind-power"]
+        self.vertical_wind = 0 if settings["wind-power"] == 0 else math.sin(settings["wind-angle"]) / settings["wind-power"]
+
+        self.landscape = generate_noise(self.num_cols, self.num_rows)
+
+        # initializing the pygame stuff
+        pygame.init()
+        self.screen = pygame.display.set_mode((self.width, self.height))
+        self.screen.fill(self.colors["black"])
+
+        # generating the grid cell with the weighted values.
+        self.grid_cell = np.array([
+            random.choices([0, 2],
+             cum_weights=(settings["density"], 1), k=self.num_cols) for i in range(self.num_rows)
+            ])
+
+        self.running = True
+        self.grid_cell[math.floor(len(self.grid_cell)/2)][math.floor(len(self.grid_cell[0])/2)] = 1
+            
+
+    def iterate(self):
+        new_grid_cell = np.copy(self.grid_cell)
+        for i in range(0, self.num_rows):
+            for j in range(0, self.num_cols):
+                if self.grid_cell[i][j] == 1:
+                    self.cell_iteration(i, j, new_grid_cell)
+
+        self.grid_cell = new_grid_cell
+
+
+    def cell_iteration(self, i, j, new_grid_cell):
+        new_grid_cell[i][j] = random.choices([3, 1], cum_weights=[self.settings["burnout"], 1], k=1)[0]
+
+        for ii in range(-1, 2):
+            if not (i == 0 and ii == -1) and not (i == len(self.grid_cell) - 1 and ii == 1):
+                for jj in range(-1, 2):
+                    if not (j == 0 and jj == -1) and not (j == len(self.grid_cell[0]) - 1 and jj == 1):
+                        if new_grid_cell[i + ii][j + jj] == 0:
+                            new_grid_cell[i + ii][j + jj] = random.choices([1, 0], cum_weights=[self.settings["ignition"], 1], k=1)[0]
+
+
+    def draw_grid(self):
+        # This iterates through every single cell to determine what color it should be based on
+        # the values in the grid_cell.
+        for i in range(self.num_cols):
+            for j in range(self.num_rows):
+
+                if self.grid_cell[j][i] == 0:
+                    self.color = self.colors["green"]
+
+                elif self.grid_cell[j][i] == 1:
+                    self.color = self.colors["red"]
+
+                elif self.grid_cell[j][i] == 2:
+                    self.color = self.colors["black"]
+
+                elif self.grid_cell[j][i] == 3:
+                    self.color = self.colors["grey"]
+                    
+                pygame.draw.rect(self.screen, self.color,
+                    pygame.Rect((i * self.block_size, j * self.block_size),
+                    (self.block_size, self.block_size))
+                )
+
+
+    def run(self):
+        while self.running:
+            self.draw_grid()
+            pygame.display.flip()
+            self.iterate()
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
