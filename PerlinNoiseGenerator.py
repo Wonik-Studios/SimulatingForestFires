e292fcae9f3325ac392f6ad47ee03c019ccca84a
MobBarley2021 <79928013+MobBarley2021@users.noreply.github.com>
2022-06-01 11:16:37 -0400

    Created PerlinNoiseGenerator

---
+++ PerlinNoiseGenerator.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+from perlin_noise import PerlinNoise
+
+
+def generate_noise(xpix, ypix):
+    noise = PerlinNoise(octaves=10, seed=1)
+    pic = [[noise([i/xpix, j/ypix]) for j in range(xpix)] for i in range(ypix)]
+    plt.imshow(pic, cmap='gray')
+    plt.show()
+    return pic
+
+if __name__ == "__main__":
+    generate_noise(800, 600)
