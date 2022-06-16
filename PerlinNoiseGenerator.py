82d902101bdc228604ced983a8943177b4a2c34a
petal-person <petalperson@wonik.tech>
2022-06-16 16:05:54 -0400

    Fixed savefig and made seed random

--- PerlinNoiseGenerator.py
+++ PerlinNoiseGenerator.py
@@ -4,12 +4,12 @@ from perlin_noise import PerlinNoise
 import os
 
 def generate_noise(xpix, ypix):
-    noise = PerlinNoise(octaves=10, seed=1)
+    noise = PerlinNoise(octaves=10)
     pic = [[noise([i/xpix, j/ypix]) for j in range(xpix)] for i in range(ypix)]
     now = datetime.now()
 
     plt.imshow(pic, cmap='gray')
-    plt.savefig(f"LandscapeGraphs{os.sep}{now.strftime('%m-%d-%Y_%H:%M:%S')}.png")
+    plt.savefig(f"LandscapeGraphs{os.sep}{now.strftime('%m-%d-%Y_%H-%M-%S')}.png")
     return pic
 
 if __name__ == "__main__":
